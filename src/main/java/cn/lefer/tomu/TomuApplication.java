6a06ddd88c7833f9d1d72f9ebdbc98b190434243
fangchao <fangchao@lefer.cn>
2020-06-15 16:40:22 +0800

    初始化项目

---
+++ src/main/java/cn/lefer/tomu/TomuApplication.java
@@ -0,0 +1,13 @@
+package cn.lefer.tomu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lefer/tomu/TomuApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lefer.tomu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
